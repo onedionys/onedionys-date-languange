89ccb6c6ff2a82dc72e956032438fc6c1838bf4b
onedionys <onedionys@gmail.com>
2024-02-26 10:27:35 +0700

    feat(date-languange): adding script to convert date to date format languange

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const langPath = './src/lang/';
+
+function formatDateLanguange(date, lang, is_cut = false) {
+    const formatDate = new Date(date);
+    const monthName = formatDate.toLocaleString('default', { month: 'long' });
+    const langData = JSON.parse(fs.readFileSync(langPath + lang + '.json', 'utf8'));
+    const monthAbbreviation = langData[monthName];
+    var cutmonthAbbreviation = monthAbbreviation;
+    if(is_cut === true) {
+        cutmonthAbbreviation = cutmonthAbbreviation.substring(0, 3);
+    }
+    return formatDate.getDate().toString().padStart(2, '0') + ' ' + cutmonthAbbreviation + ' ' + formatDate.getFullYear();
+}
+
+module.exports = { formatDateLanguange };
